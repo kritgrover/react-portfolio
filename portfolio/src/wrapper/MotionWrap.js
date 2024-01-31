9b0ec2975a29edbff8c2a646a543ee68edb294bc
kritgrover <kritgrover@gmail.com>
2024-01-31 09:44:07 -0500

    minor style changes

--- portfolio/src/wrapper/MotionWrap.js
+++ portfolio/src/wrapper/MotionWrap.js
@@ -6,7 +6,7 @@ const MotionWrap = (Component, classNames) => function HOC() {
     <AnimatePresence>
       <motion.div
         whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
-        transition={{ duration: 0.8, ease: 'easeIn' }}
+        transition={{ duration: 0.8, ease: 'easeOut' }}
         className={`${classNames} app__flex`}
       >
         <Component />
