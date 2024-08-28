246ae688c9537d14804a435485182d943cb1333a
kritgrover <kritgrover@gmail.com>
2024-08-28 18:45:59 -0400

    Updated social media link

--- portfolio/src/components/SocialMedia.jsx
+++ portfolio/src/components/SocialMedia.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
-import { BsLinkedin, BsInstagram, BsGithub} from 'react-icons/bs'
+import React from "react";
+import { BsLinkedin, BsMedium, BsGithub } from "react-icons/bs";
 
 const SocialMedia = () => {
   return (
-    <div className='app__social'>
-        <a href='https://www.linkedin.com/in/kritgrover/' rel='noreferrer' target='_blank'><div><BsLinkedin /></div></a>
-        <a href='https://www.instagram.com/_krizgrover_/' rel='noreferrer' target='_blank'><div><BsInstagram /></div></a>
-        <a href='https://www.github.com/kritgrover/' rel='noreferrer' target='_blank'><div><BsGithub /></div></a>
+    <div className="app__social">
+      <a
+        href="https://www.linkedin.com/in/kritgrover/"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <div>
+          <BsLinkedin />
+        </div>
+      </a>
+      <a
+        href="https://www.medium.com/kritgrover/"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <div>
+          <BsMedium />
+        </div>
+      </a>
+      <a
+        href="https://www.github.com/kritgrover/"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <div>
+          <BsGithub />
+        </div>
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;
